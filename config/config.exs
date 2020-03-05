af8c84a754b305271c8cc3dc1730d440c3faae3e
kiote <krivich.ekaterina@gmail.com>
2020-03-05 17:27:12 +0200

    format

--- config/config.exs
+++ config/config.exs
@@ -23,11 +23,12 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
-config :darkskyx, api_key: System.get_env("DARKSKY_API_KEY"),
+config :darkskyx,
+  api_key: System.get_env("DARKSKY_API_KEY"),
   defaults: [
-      units: "us",
-      lang: "en"
-    ]
+    units: "us",
+    lang: "en"
+  ]
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
--- lib/simple_weather_web/controllers/page_controller.ex
+++ lib/simple_weather_web/controllers/page_controller.ex
@@ -1,5 +1,6 @@
 defmodule SimpleWeatherWeb.PageController do
   use SimpleWeatherWeb, :controller
+
   def index(conn, _params) do
     text(conn, "HI")
   end
