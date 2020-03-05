b66be4bd2b4c0a049a4865a5bd756ab103531821
kiote <ekaterinak@heathmont.net>
2020-03-05 17:32:07 +0200

    lib from hex

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule SimpleWeather.MixProject do
       {:phoenix_pubsub, "~> 1.1"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:darkskyx, path: "../darkskyx"}
+      {:darkskyx, "~> 1.0"}
     ]
   end
 end
