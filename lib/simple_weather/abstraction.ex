720dd60f05f443cdf8a8bee8972c83ff39d4db67
kiote <krivich.ekaterina@gmail.com>
2020-04-12 17:23:37 +0300

    till dark

--- lib/simple_weather/abstraction.ex
+++ lib/simple_weather/abstraction.ex
@@ -1,12 +1,14 @@
 defmodule SimpleWeather.Abstraction do
+  require Logger
+
   alias SimpleWeather.Types.Condition
   alias SimpleWeather.Types.ShortCondition
   alias SimpleWeather.Types.Today
 
   @spec today(any()) :: %Today{morning: %Condition{}, evening: %Condition{}, till_dark: integer()}
   def today(implementation) do
-    {:ok, today, headers} = implementation.today()
-    till_dark = get_till_dark_from(today)
+    {:ok, today, _headers} = implementation.today()
+    _till_dark = get_till_dark_from(today)
   end
 
   @spec days_ago(days: non_neg_integer(), implementation: any()) :: %ShortCondition{}
@@ -16,6 +18,11 @@ defmodule SimpleWeather.Abstraction do
 
   # private
 
-  defp get_till_dark_from(today) do
+  defp get_till_dark_from(%{daily: %{data: []}}) do
+    Logger.error("Unexpected: empty daily data")
+  end
+
+  defp get_till_dark_from(%{daily: %{data: [%{sunsetTime: sunset_time} | _]}}) do
+    sunset_time
   end
 end
