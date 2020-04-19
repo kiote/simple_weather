da5c0129eb2d3b2228e7a0472a4fa1caf2d69210
kiote <krivich.ekaterina@gmail.com>
2020-04-19 17:19:14 +0300

    fix

--- lib/utils/strings_to_atoms.ex
+++ lib/utils/strings_to_atoms.ex
@@ -2,7 +2,7 @@ defmodule Utils.StringsToAtoms do
   require Logger
 
   def convert(string_key_map) when is_map(string_key_map) do
-    for {key, val} <- string_key_map, into: %{}, do: {String.to_atom(key), convert(val)}
+    for {key, val} <- string_key_map, into: %{}, do: do_convert(key, val)
   end
 
   def convert(string_key_list) when is_list(string_key_list) do
@@ -10,4 +10,12 @@ defmodule Utils.StringsToAtoms do
   end
 
   def convert(value), do: value
+
+  defp do_convert(key, val) when is_binary(key) do
+    {String.to_atom(key), convert(val)}
+  end
+
+  defp do_convert(key, val) do
+    {key, convert(val)}
+  end
 end
