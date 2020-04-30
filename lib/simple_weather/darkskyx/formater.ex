dea05c9aa477e8db1eca1cea0f2c2936dcdbe91b
kiote <krivich.ekaterina@gmail.com>
2020-04-30 20:44:48 +0300

    update

--- lib/simple_weather/darkskyx/formater.ex
+++ lib/simple_weather/darkskyx/formater.ex
@@ -11,29 +11,41 @@ defmodule SimpleWeather.Darkskyx.Formater do
 
   def get_till_dark_from_now(forecast) do
     weather_time().now()
-    |> Kernel.-(sunset_time(forecast))
+    |> deduct_sunset_time(forecast)
     |> weather_time().to_hours()
-    |> Kernel.abs()
+    |> change_sign()
   end
-
-  # %{temperature: t1, precipProbability: p1, windSpeed: w1, time: tm1},
-  # %{temperature: t2, precipProbability: p2, windSpeed: w2, time: tm2},
-  # %{temperature: t3, precipProbability: p3, windSpeed: w3, time: tm3} | _
 
   def get_weather_condition(%{hourly: %{data: data}} = forecast) do
     sunset_time = sunset_time(forecast)
 
-    before_dusk =
-      data
-      |> Enum.filter(fn %{time: t} -> t < sunset_time end)
+    before_dusk = filter_dark_hours(data, sunset_time)
 
     %Condition{
       precipitation_probability:
-        Enum.max(Enum.map(before_dusk, fn %{precipProbability: p} -> p end)),
-      wind: Enum.max(Enum.map(before_dusk, fn %{windSpeed: w} -> w end)),
-      temperature: Enum.max(Enum.map(before_dusk, fn %{temperature: t} -> t end))
+        before_dusk
+        |> get_list_of(:precipProbability)
+        |> Enum.max(),
+      wind: 
+        before_dusk
+        |> get_list_of(:windSpeed)
+        |> Enum.max(),
+      temperature: 
+        before_dusk
+        |> get_list_of(:temperature)
+        |> Enum.min()
     }
   end
 
   defp sunset_time(%{daily: %{data: [%{sunsetTime: sunset_time} | _]}}), do: sunset_time
+
+  defp deduct_sunset_time(timestamp, forecast), do: timestamp - sunset_time(forecast)
+
+  defp change_sign(x), do: -x
+
+  defp filter_dark_hours(d, sunset_time), do: Enum.filter(d, fn %{time: t} -> t < sunset_time end)
+
+  defp get_list_of(d, key) do
+    Enum.map(d, fn %{^key => p} -> p end)
+  end
 end
