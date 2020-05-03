0bf6d8049927b995d09e8d91941a895346e3ffe8
kiote <krivich.ekaterina@gmail.com>
2020-05-03 14:50:53 +0300

    add test

---
+++ test/utils/weather_time_test.exs
@@ -0,0 +1,13 @@
+defmodule SimpleWeather.Utils.WeatherTimeTest do
+  use ExUnit.Case, async: true
+
+  alias SimpleWeather.Utils.WeatherTime
+
+  test "now" do
+    assert WeatherTime.now() == Timex.now |> Timex.to_unix()
+  end
+
+  test "to_hours/1" do
+    assert 2.0 == WeatherTime.to_hours(7200)
+  end
+end
