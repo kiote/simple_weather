6362885fe186859a0526f71472878c1ec67ec5fd
kiote <krivich.ekaterina@gmail.com>
2020-05-03 22:32:25 +0300

    More tests

--- test/simple_weather/cache_test.exs
+++ test/simple_weather/cache_test.exs
@@ -2,6 +2,7 @@ defmodule SimpleWeather.CacheTest do
   use ExUnit.Case, async: true
 
   alias SimpleWeather.Cache
+  alias SimpleWeather.Utils.EtsCache
 
   defmodule TestAdapter do
     def get_cache_key(_) do
@@ -9,10 +10,25 @@ defmodule SimpleWeather.CacheTest do
     end
   end
 
+  def clear_cache() do
+    EtsCache.clear()
+  end
+
+  setup do
+    clear_cache()
+    :ok
+  end
+
   describe "maybe_get_from/3" do
     test "first call returns function value" do
       func = fn -> "value" end
+      assert "value" == Cache.maybe_get_from("params", TestAdapter, func)
+    end
 
+    test "two calls return cached value" do
+      func = fn -> "value" end
+      assert "value" == Cache.maybe_get_from("params", TestAdapter, func)
+      func = nil
       assert "value" == Cache.maybe_get_from("params", TestAdapter, func)
     end
   end
@@ -20,7 +36,13 @@ defmodule SimpleWeather.CacheTest do
   describe "maybe_get_from/4" do
     test "first call returns function value" do
       func = fn _ -> "value" end
+      assert "value" == Cache.maybe_get_from("params", TestAdapter, func, {})
+    end
 
+    test "two calls return cached value" do
+      func = fn _ -> "value" end
+      assert "value" == Cache.maybe_get_from("params", TestAdapter, func, {})
+      func = nil
       assert "value" == Cache.maybe_get_from("params", TestAdapter, func, {})
     end
   end
